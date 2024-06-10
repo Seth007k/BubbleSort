3dab4234b7e81d31d5a589120f6bb9d4c214b105
Seth007k <107913274+Seth007k@users.noreply.github.com>
2024-06-10 12:37:57 +0200

    Add files via upload

---
+++ BubbleSortExample.java
@@ -0,0 +1,43 @@
+public class BubbleSortExample {
+
+    public BubbleSortExample(){}
+
+    public int [] bubbleSort(int[] array) {
+        int temp;
+        for(int i = 0; i < array.length - 1; i++){
+
+            for(int j = 0; j < array.length - 1; j++) {
+                if(array[j] > array[j+1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+     public static void main(String[] args) {
+
+         int[] array = {7,11,4,18,1,9,8,2,12,19,16,5,15};
+
+         System.out.println("Unsortiert\n");
+         for(int i = 0; i < array.length;i++){
+             System.out.println(array[i]);
+         }
+
+         System.out.println("\n");
+         System.out.println("Sortiert\n");
+
+         BubbleSortExample bubbleSortExample = new BubbleSortExample();
+         int[] sortiert = bubbleSortExample.bubbleSort(array);
+
+         for(int i = 0; i < sortiert.length; i++){
+             System.out.println(sortiert[i]);
+         }
+     }
+
+
+
+
+
+}
